e500393ef9f7b8b6e573573ccf72c169f9b33ecb
Jonhatan Morón <jonidimoni23@hotmail.com>
2022-02-17 20:25:27 +0100

    cambios

--- app/models/tracks.py
+++ app/models/tracks.py
@@ -1,16 +1,15 @@
 from app.extensions.database import BaseModelMixin, db
-from app.models import Track  # noqa:F401
 
 
-class track(db.Model, BaseModelMixin):
+class Track(db.Model, BaseModelMixin):
     __tablename__ = "Track"
-    id = db.Column(name="trackId", type_=db.Integer, primary_key=True, autoincrement=True)
+    id = db.Column(name="TrackId", type_=db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(name="Name", type_=db.Unicode(200), nullable=False)
-    media_type_id = db.Column(name="media_type_id", type=db.Integer, nullable=False, default=1)
+    media_type_id = db.Column(name="MediaTypeId", type_=db.Integer, nullable=False, default=1)
     name = db.Column(name="Composer", type_=db.Unicode(220), nullable=False)
     id = db.Column(name="Miliseconds", type_=db.Integer, nullable=False)
     id = db.Column(name="Bytes", type_=db.Integer, default=None)
-    unit_price = db.Column(name="Unitprice", type=db.float)
+    unit_price = db.Column(name="UnitPrice", type_=db.Numeric(10, 2))
 
     def __repr__(self):
         return f"<Tracks {self.name}>"
