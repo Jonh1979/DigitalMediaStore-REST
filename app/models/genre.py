36aec0e7e8dfd7836465c279ca036a06934dc76c
Jonhatan Morón Ojalvo <Jonidimoni23@hotmail.com>
2022-02-27 02:24:53 +0100

    Merge branch 'master' of https://github.com/Jonh1979/DigitalMediaStore-REST

--- app/models/genre.py
+++ app/models/genre.py
@@ -1,14 +1,13 @@
 from app.extensions.database import BaseModelMixin, db
-from app.models import genre
 
 
-class track(db.Model, BaseModelMixin):
+class Genre(db.Model, BaseModelMixin):
     __tablename__ = "Track"
     id = db.Column(name="GenreId", type_=db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(name="Name", type_=db.Unicode(120), nullable=False, unique=True)
 
     def __repr__(self):
-        return f"<Tracks {self.name}>"
+        return f"<genre {self.name}>"
 
     def __str__(self):
         return self.name
@@ -18,5 +17,5 @@ class track(db.Model, BaseModelMixin):
     #     self.id
 
     @classmethod
-    def find_track_by_name(cls, name):
+    def find_genre_by_name(cls, name):
         return cls.simple_filter(name=name).first()
